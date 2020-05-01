a5a8ef2f4d3d0d4335be7c36289694c909e01f2b
Boris2209 <boris.litvinenko@bk.ru>
2020-05-01 20:40:05 +0300

    Изменил title

--- Main.py
+++ Main.py
@@ -3,7 +3,7 @@ from Form import *
 # окно приложения
 window = Tk()
 window.geometry("1200x900")
-window.title("Graph")
+window.title("Сиаод, 3 лабораторная, Литвненко Борис ИКБО-06-18")
 g = Graph()     # граф с которым производятся действия
 w = Form(window, g)     # создаю форму и указываю с каким графом надо работать
 window.mainloop()
